cf51659f148bba4576e6984e6dc1ceea2a628636
rtolkacheva <r.tolkacheva@g.nsu.ru>
2022-08-20 03:25:31 +0700

    Update target test_sql so it finally works.

--- test/fuzz/sql_fuzzer/main.cc
+++ test/fuzz/sql_fuzzer/main.cc
@@ -1,103 +1,33 @@
-#include <cstdio>
-#include <string>
 #include <iostream>
+#include <cstring>
 
-#include "box/box.h"
-#include "box/error.h"
-#include "box/execute.h"
-#include "box/iproto.h"
-#include "box/memtx_tx.h"
-#include "box/module_cache.h"
-#include "box/sql.h"
-#include "cbus.h"
-#include "cfg.h"
-#include "coio_task.h"
-#include "coll/coll.h"
-#include "core/crash.h"
-#include "core/ssl.h"
-#include "core/popen.h"
-#include <crc32.h>
-#include "engine.h"
-#include "fiber.h"
-#include "lua/init.h"
-#include "main.h"
-#include "memory.h"
-#include "random.h"
-#include "replication.h"
-#include "schema.h"
-#include "systemd.h"
-#include "trivia/util.h"
-#include "utils.h"
+#include <tarantool/tarantool.h>
+#include <tarantool/tnt_net.h>
+#include <tarantool/tnt_opt.h>
 
-void my_box_cfg(void)
-{
-    try
-    {
-        engine_init();
-        schema_init();
-        replication_init(cfg_geti_default("replication_threads", 1));
-        port_init();
-        iproto_init(cfg_geti("iproto_threads"));
-        sql_init();
-        // box_set_net_msg_max();
-        // box_set_readahead();
-        // box_set_too_long_threshold();
-        // box_set_replication_timeout();
-        // box_set_replication_connect_timeout();
-        // box_set_replication_connect_quorum();
-        // box_set_replication_sync_lag();
-        // if (box_set_replication_synchro_quorum() != 0)
-        //     diag_raise();
-        //     diag_raise();
-        // box_set_replication_sync_timeout();
-        // box_set_replication_skip_conflict();
-        // box_set_replication_anon();
-    }
-    catch (Exception *e)
-    {
-        e->log();
-        panic("can't initialize storage: %s", e->get_errmsg());
-    }
-}
+int main() {
+    const char *query = "CREATE TABLE t (s1 SCALAR PRIMARY KEY)";
+    auto args = tnt_object(NULL);
+	tnt_object_format(args, "[]");
 
-int main(int argc, char **argv)
-{
-    start_time = ev_monotonic_time();
-    random_init(); //+
-    crc32_init();  //+
-    memory_init(); //+
-    exception_init();
-    fiber_init(fiber_cxx_invoke); //+
-    popen_init();                 //+
-    cbus_init();                  //+
-    coll_init();                  //+
-    memtx_tx_manager_init();      //+
-    module_init();                //+
-    ssl_init();                   //+
-    systemd_init();               //+
-    box_init();                   //+
+    const char *uri = "localhost:3301";
+    struct tnt_stream *tnt = tnt_net(NULL); // Allocating stream
+    tnt_set(tnt, TNT_OPT_URI, uri); // Setting URI
+    tnt_set(tnt, TNT_OPT_SEND_BUF, 0); // Disable buffering for send
+    tnt_set(tnt, TNT_OPT_RECV_BUF, 0); // Disable buffering for recv
+    int connection_result = tnt_connect(tnt); // Initialize stream and connect to Tarantool
+    std::cout << connection_result << std::endl;
 
-    my_box_cfg();
+    ssize_t execute_result = tnt_execute(tnt, query, strlen(query), args);
+    std::cout << execute_result << std::endl;
 
-    std::string query = "DROP TABLE t31;";
+    struct tnt_reply *reply = tnt_reply_init(NULL); // Initialize reply
+    int reply_result = tnt->read_reply(tnt, reply); // Read reply from server
+    std::cout << reply_result << std::endl;
+    std::cout << reply->error << "\n" << reply->sqlinfo << "\n" << reply->metadata << "\n" 
+        << reply->data << std::endl;
 
-    struct port port;
-    int result = sql_prepare_and_execute(query.data(), query.length(),
-                                         nullptr, 0, &port, &fiber()->gc);
-    printf("%d\n", result);
-
-    // auto expr = sql_expr_compile(sql_get(), query.data(), query.length());
-    // std::cout << expr << std::endl;
-
-    box_free();
-    systemd_free();
-    ssl_free();
-    module_free();
-    memtx_tx_manager_free();
-    coll_free();
-    cbus_free();
-    popen_free();
-    fiber_free();
-    memory_free();
-    random_free();
+    tnt_reply_free(reply); // Free reply
+    tnt_stream_free(args);
+    tnt_close(tnt); tnt_stream_free(tnt); // Close connection and free stream object
 }
--- test/fuzz/sql_fuzzer/sql_fuzzer.cc
+++ test/fuzz/sql_fuzzer/sql_fuzzer.cc
@@ -3,27 +3,23 @@
 
 #include <libprotobuf-mutator/port/protobuf.h>
 #include <libprotobuf-mutator/src/libfuzzer/libfuzzer_macro.h>
-#include "box/sql/sqlInt.h"
-#include "tarantoolInt.h"
-#include "box/box.h"
-#include "box/sql.h"
 #include "sql_query.pb.h"
 #include "sql_query_proto_to_string.h"
 
 DEFINE_PROTO_FUZZER(const sql_query::SQLQuery& query) {
-    std::string query_str = sql_fuzzer::SQLQueryToString(query);
+    // std::string query_str = sql_fuzzer::SQLQueryToString(query);
 
-    if (::getenv("LPM_DUMP_NATIVE_INPUT") && query_str.size() != 0) {
-        std::cout << "_________________________" << std::endl;
-        std::cout << query_str << std::endl;
-        std::cout << "------------------------" << std::endl;
-    }
+    // if (::getenv("LPM_DUMP_NATIVE_INPUT") && query_str.size() != 0) {
+    //     std::cout << "_________________________" << std::endl;
+    //     std::cout << query_str << std::endl;
+    //     std::cout << "------------------------" << std::endl;
+    // }
 
-    sql_init();
-    struct sql *db = sql_get();
-    Parse sParse;
-    sql_parser_create(&sParse, db, current_session()->sql_flags);
+    // sql_init();
+    // struct sql *db = sql_get();
+    // Parse sParse;
+    // sql_parser_create(&sParse, db, current_session()->sql_flags);
 
-    sqlRunParser(&sParse, query_str.data());
-    sql_parser_destroy(&sParse);
+    // sqlRunParser(&sParse, query_str.data());
+    // sql_parser_destroy(&sParse);
 }
